95e97d7951cbab0061e7b9684f53e6882eb3088d
jogax <jozsef.romhanyi@gmail.com>
2015-07-11 21:07:26 +0200

    Collosionen V1 (not working)

--- JSprites/GameScene.swift
+++ JSprites/GameScene.swift
@@ -8,26 +8,75 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
+
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Container : UInt32 = 0b1       // 1
+    static let Sprite    : UInt32 = 0b10      // 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     let images = DrawImages()
     let maxGeneratedColorCount = 10
     let countContainers = 4
     var timer: NSTimer?
     var containers = [SKSpriteNode]()
     var countColorsProContainer = [Int]()
+    var movedFromNode: SKNode!
     
     override func didMoveToView(view: SKView) {
         let xDelta = size.width / CGFloat(countContainers)
         for index in 0..<countContainers {
             let aktColor = GV.colorSets[GV.colorSetIndex][index + 1].CGColor
             let containerTexture = SKTexture(image: images.drawCircle(CGSizeMake(50,50), imageColor: aktColor))
+            let centerX = (size.width / CGFloat(countContainers)) * CGFloat(index) + xDelta / 2
+            let centerY = size.height * 0.85
             containers.append(SKSpriteNode(texture: containerTexture))
-            let xPoint = (size.width / CGFloat(countContainers)) * CGFloat(index) + xDelta / 2
-            containers[index].position = CGPoint(x: xPoint, y: size.height * 0.85)
+            containers[index].position = CGPoint(x: centerX, y: centerY)
+            containers[index].name = "\(index)"
+            containers[index].physicsBody = SKPhysicsBody(rectangleOfSize: containers[index].size) // 1
+            containers[index].physicsBody?.dynamic = true // 2
+            containers[index].physicsBody?.categoryBitMask = PhysicsCategory.Container // 3
+            containers[index].physicsBody?.contactTestBitMask = PhysicsCategory.Sprite // 4
+            containers[index].physicsBody?.collisionBitMask = PhysicsCategory.None // 5
             countColorsProContainer.append(maxGeneratedColorCount)
             addChild(containers[index])
         }
         backgroundColor = UIColor.whiteColor() //SKColor.whiteColor()
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
         generateSprite()
     }
     func generateSprite() {
@@ -48,42 +97,53 @@ class GameScene: SKScene {
             let xPosition = size.width * CGFloat(GV.random(20, max: 80)) / 100
             let yPosition = size.height * CGFloat(GV.random(20, max: 80)) / 100
             sprite.position = CGPoint(x: xPosition, y: yPosition)
-            //points.append(point)
-            //point!.backgroundColor = playColors[color]
-            //point!.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+            sprite.name = "\(10000 + 100 * (colorIndex) + maxGeneratedColorCount - countColorsProContainer[colorIndex])"
+            let generatedName = sprite.name
             addChild(sprite)
-            //point.layer.name = "\(points.count)"
-            
-            //println("Anzahl Sprites: \(MySprite.spritesCount)")
-            //setupLayoutForPoint(point!)
         } else {
-            /*
-            var gameEndAlert:UIAlertController?
-            var messageTxt = GV.language.getText("restart")
-            gameEndAlert = UIAlertController(title: GV.language.getText("timeout"),
-                message: messageTxt,
-                preferredStyle: .Alert)
-            
-            let OKAction = UIAlertAction(title: "OK",
-                style: UIAlertActionStyle.Default,
-                handler: {(paramAction:UIAlertAction!) in
-                    //self.firstGameView!.restart()
-                    //self.timeLeft = self.timeLeftOrig
-                    GV.timeCount = 0
-                    GV.timeAdder = 1
-                }
-            )
-            
-            
-            //timeEndAlert!.addAction(OKAction)
-            presentViewController(timeEndAlert!,
-                animated:true,
-                completion: nil)
-     */       
+        }
+    }
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let countTouches = touches.count
+        let firstTouch = touches.first as! UITouch
+        let touchLocation = firstTouch.locationInNode(self)
+        let node = self.nodeAtPoint(touchLocation)
+        if node.name?.toInt() >= 10000 {
+            movedFromNode = self.nodeAtPoint(touchLocation)
+        } else {
+            movedFromNode = nil
         }
         
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if movedFromNode != nil {
+            let countTouches = touches.count
+            let firstTouch = touches.first as! UITouch
+            let touchLocation = firstTouch.locationInNode(self)
 
+            let node = self.nodeAtPoint(touchLocation)
+            node.physicsBody = SKPhysicsBody(circleOfRadius: node. .size.width/2)
+            node.physicsBody?.dynamic = true
+            node.physicsBody?.categoryBitMask = PhysicsCategory.Sprite
+            node.physicsBody?.contactTestBitMask = PhysicsCategory.Container
+            node.physicsBody?.collisionBitMask = PhysicsCategory.None
+            node.physicsBody?.usesPreciseCollisionDetection = true
+            let offset = touchLocation - movedFromNode.position
 
+            let direction = offset.normalized()
+            
+            // 7 - Make it shoot far enough to be guaranteed off screen
+            let shootAmount = direction * 1000
+            
+            // 8 - Add the shoot amount to the current position
+            let realDest = shootAmount + movedFromNode.position
+            
+            // 9 - Create the actions
+            let actionMove = SKAction.moveTo(realDest, duration: 2.0)
+            let actionMoveDone = SKAction.removeFromParent()
+            movedFromNode.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        }
     }
 
 }
